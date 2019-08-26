9e50e0c239517669d2066eb202a972be2bacd1b9
AEdwards <valefleur@gmail.com>
2019-08-25 19:12:43 -0700

    Basic home page

--- webapp.py
+++ webapp.py
@@ -2,9 +2,21 @@ from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return "Hello World!"
+    return render_template('home.html')
+
+
+@app.route('/learn')
+def learn_more():
+    return 'This is the Learn More page! It is still TBD.'
+
+
+@app.route('/book')
+def book_a_class():
+    return 'This is the Book a Class page! It is still TBD.'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
