df3dc6dbc9170edd8ab006738444810315b4be19
AEdwards <valefleur@gmail.com>
2019-08-25 19:35:07 -0700

    Added GET method

--- webapp.py
+++ webapp.py
@@ -3,22 +3,22 @@ from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
 
-@app.route('/learn')
+@app.route('/learn', methods=['GET'])
 def learn_more():
     return 'This is the Learn More page! It is still TBD.'
 
 
-@app.route('/book')
+@app.route('/book', methods=['GET'])
 def book_a_class():
     return 'This is the Book a Class page! It is still TBD.'
 
 
-@app.route('/pics')
+@app.route('/pics', methods=['GET'])
 def pictures():
     return 'This is the Pictures page! It is still TBD.'
 
