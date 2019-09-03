2bb62849aeaf9fc7480ef70766c3781a1df243dc
AEdwards <valefleur@gmail.com>
2019-09-03 08:37:49 -0700

    Started moving to a production server (gunicorn).

--- webapp.py
+++ webapp.py
@@ -1,6 +1,26 @@
 from flask import Flask, render_template, url_for, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Init and define schema for db
+db = SQLAlchemy(app)
+
+
+class poses(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # image = db.Column(db.Blob) # TODO how to store an image
+    # TODO how expensive are images in Heroku?  Cheaper alternatives?
+    # Expecting ~25 images in db
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(1000), nullable=False)
+    stretched = db.Column(db.String(500))
+    worked = db.Column(db.String(500))
+
+    def __repr__(self):
+        return '<Task %r>; % self.id'
 
 
 @app.route('/', methods=['GET'])
